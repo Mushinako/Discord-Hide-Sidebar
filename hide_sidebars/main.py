0f7e87b2c7608ca92677ee6c5ef8b630e11cfd74
Mushinako <ridoedee@gmail.com>
2020-09-27 10:16:45 -0700

    Argparse and object initiation

--- hide_sidebars/main.py
+++ hide_sidebars/main.py
@@ -2,13 +2,83 @@
 """Main module. Decides which functions to call"""
 
 import platform
+from argparse import ArgumentParser, Namespace
+from typing import Optional
+
+from hide_sidebars.runner_obj import WinDiscordHideSidebar
+
+DEFAULT_JS_PATH = "inject.js"
+
+
+class DiscordHideSidebarArgs(Namespace):
+    """Specific `Namespace` object for arguments of this program
+    Mainly used for type notation
+
+    Instance variables:
+        discord_path [Optional[str]]: Path of Discord executable
+        js_path      [Optional[str]]: Path of JavaScript to be executed
+        port         [Optional[int]]: Port for the debugging session to run
+    """
+
+    def __init__(
+        self,
+        discord_path: Optional[str],
+        js_path: Optional[str],
+        port: Optional[int]
+    ) -> None:
+        self.discord_path = discord_path
+        self.js_path = js_path
+        self.port = port
+
+
+def parse_arguments() -> DiscordHideSidebarArgs:
+    """Parse command line arguments
+
+    Returns:
+        [DiscordHideSidebarArgs]: The object containing all arguments
+    """
+    parser = ArgumentParser(description="Hide sidebars on Discord!")
+    parser.add_argument(
+        "-d", "--discord-path",
+        nargs=1,
+        default=None,
+        help="Path of Discord executable",
+        dest="discord_path"
+    )
+    parser.add_argument(
+        "-j", "--js-path",
+        nargs=1,
+        default=None,
+        help="Path of JavaScript to be executed",
+        dest="js_path"
+    )
+    parser.add_argument(
+        "-p", "--port",
+        nargs=1,
+        default=None,
+        type=int,
+        help="Port for the debugging session to run",
+        dest="port"
+    )
+    args = parser.parse_args(namespace=DiscordHideSidebarArgs)
+    return args
 
 
 def main() -> None:
-    """Main function. Checks OS and runs respective code"""
+    """Main function
+    Gets command line options
+    Checks OS
+    Runs respective code
+    """
+
+    args = parse_arguments()
 
     if platform.platform == "Windows":
-        pass
+        runner = WinDiscordHideSidebar(
+            args.discord_path,
+            args.js_path,
+            args.port
+        )
     else:
         raise NotImplementedError(
             f"Your operating system \"{platform.platform}\" is not yet supported"
--- hide_sidebars/runner_obj.py
+++ hide_sidebars/runner_obj.py
@@ -4,17 +4,29 @@ import os.path
 import glob
 from typing import List, Optional
 
+JS_FILE_NAME = "inject.js"
+
 
 class DiscordHideSidebar:
     """Base class for all operating systems
 
     Class variables:
-        DEFAULT_DISCORD_PATH [str]: Default path for Discord executable
-        DEFAULT_JS_PATH      [str]: Default path for JavaScript to be run
-        DEFAULT_PORT         [int]: Default port for the debugging session to run
+        DEFAULT_DISCORD_PATH_PATTERN [str]: Glob pattern of default path of Discord executable
+        DEFAULT_JS_PATH              [str]: Default path of JavaScript to be run
+        DEFAULT_PORT                 [int]: Default port for the debugging session to run
+        COMMAND_PARAMETERS           [str]: Command parameters to trigger Discord debugging mode
+
+    Instance variables:
+        discord_path [str]: Path of Discord executable
+        js_path      [str]: Path of JavaScript
+        port         [int]: Port forthe debugging session to run
     """
-    DEFAULT_DISCORD_PATH: str = ""
-    DEFAULT_JS_PATH: str = ""
+    DEFAULT_DISCORD_PATH_PATTERN: str = ""
+    DEFAULT_JS_PATH: str = os.path.join(
+        os.path.dirname(__file__),
+        os.path.pardir,
+        JS_FILE_NAME
+    )
     DEFAULT_PORT: str = 34726
     COMMAND_PARAMETERS: str = "--remote-debugging-port={port}"
 
@@ -27,15 +39,24 @@ class DiscordHideSidebar:
         return object.__new__(cls, *args, **kwargs)
 
     def __init__(self, discord_path: Optional[str], js_path: Optional[str], port: Optional[int]) -> None:
-        raise NotImplementedError()
+        if not self.DEFAULT_DISCORD_PATH:
+            raise NotImplementedError(
+                f"This class {type(self).__name__} is not fully implemented!"
+            )
+        self.discord_path = discord_path
+        if self.discord_path is None:
+            # The last match is assumed to be the latest version
+            self.discord_path = glob.glob(
+                self.DEFAULT_DISCORD_PATH_PATTERN
+            )[-1]
+        # Check if Discord path is correct
+        self.js_path = js_path or self.DEFAULT_JS_PATH
+        self.port = port or self.DEFAULT_PORT
 
 
 class WinDiscordHideSidebar(DiscordHideSidebar):
     """Special variables/functions for Windows"""
-    DEFAULT_DISCORD_PATH = os.path.join(
+    DEFAULT_DISCORD_PATH_PATTERN = os.path.join(
         os.path.expandvars(r"%LocalAppData%"),
         r"Discord\*\Discord.exe"
     )
-
-
-pass
