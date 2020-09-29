bf14a92be148f3e5bbb8d706211cecf0340bd796
Mushinako <ridoedee@gmail.com>
2020-09-29 15:03:02 -0700

    Autostart script draft

--- autostartreg.bat
+++ autostartreg.bat
@@ -1,8 +1,44 @@
 @echo off
-set script=hideside.vbs
-set tmpreg=tmp.reg
-set folder=%~dp0
-set scriptpath="%folder%%script%"
-set tmpregpath="%folder%%tmpreg%"
+set triggerstr="ptb"
+
+set scriptdir=%~dp0
+set scriptname=hideside.vbs
+set scriptpath="%scriptdir%%scriptname%"
+
 echo %scriptpath%
-echo %tmpregpath%
+
+set regkey="HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+set regname="Discord"
+set regnameptb="DiscordPTB"
+
+set discordtmppath="discord.tmp"
+set discordptbtmppath="discordptb.tmp"
+
+set addptb=1
+
+@REM Check existence of Discord
+reg query %regkey% /v %regname% > %discordtmppath% 2>&1 || goto cleanup
+for /f "usebackq tokens=3" %%a in (%discordtmppath%) do (
+    echo "Hi"
+    echo %%a
+)
+goto ptb
+
+:cleanup
+del %discordtmppath%
+
+:ptb
+@REM Check existence of DiscordPTB
+reg query %regkey% /v %regnameptb% > %discordptbtmppath% 2>&1 || goto ptbcleanup
+for /f "usebackq tokens=3" %%a in (%discordptbtmppath%) do (
+    echo "Hi PTB"
+    echo %%a
+)
+goto end
+
+:ptbcleanup
+del %discordptbtmppath%
+
+:end
+if %1==ptb echo var1: %1
+exit
